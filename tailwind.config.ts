44ca48283279c9cd5f643a69f17cfd5fb9c2d72c
Fernando Lara Amaral <37773885+fernandolaraamaral@users.noreply.github.com>
2023-11-17 11:51:03 -0300

    .

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,41 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      white: '#FFFFFF',
+      black: '#000000',
+
+      'green-100': '#50B2C0',
+      'green-200': '#255D6A',
+      'green-300': '#0A313C',
+
+      'purple-100': '#8381D9',
+      'purple-200': '#2A2879',
+
+      'gray-100': '#F8F9FC',
+      'gray-200': '#E6E8F2',
+      'gray-300': '#D1D6E4',
+      'gray-400': '#8D95AF',
+      'gray-500': '#303F73',
+      'gray-600': '#252D4A',
+      'gray-700': '#181C2A',
+      'gray-800': '#0E1116',
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: 'var(--font-nunito)',
       },
+      backgroundImage: {
+        'gradient-vertical':
+          'linear-gradient(180deg, #7FD1CC 0%, #9694F5 100%)',
+        'gradient-horizontal': `linear-gradient(90deg, #7FD1CC 0%, #9694F5 100%)`,
+      },
+      width: {
+        '372': '23.25rem',
+      },
+      // borderRadius: {
+      //   lg: '.625rem',
+      // },
     },
   },
   plugins: [],
